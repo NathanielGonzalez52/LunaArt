07277c142851818c4887e54147a84b295953c0d1
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2023-12-19 20:49:58 -0800

    Correct spacing

--- app/about/page.js
+++ app/about/page.js
@@ -1,57 +1,64 @@
+"use client";
 import React from 'react';
 import "./page.css";
 import Navbar from "../navbar";
 import Footer from "../footer";
+import Fade from 'react-reveal/Fade';
+
 
 
 export default function About() {
   return (
     <>
     <Navbar />
-    <div id="about" className="about">
-      <div className="about-wel">
-        <h1 className="top-about">About Me</h1>
-      </div>
-      <div className="about-images">
-        <img className="about-pic" alt="mews" src="../luna_images/latios.jpg"></img>
-        <img className="about-pic" alt="groudon" src="../luna_images/groudon.jpg"></img>
-        <img className="about-pic" alt="latias" src="../luna_images/latias.jpg"></img>
-      </div>
-      <h1 className='about-header title'>Who Am I?</h1>
-      <p>I am a full-time independent illustrator and designer. 
-      I also freelance for publishing, animation, and games.</p>
-      <div className="about-wel title">
-        <h2>Things You May Know Me From...</h2>
-      </div>
-      <p>I was a background painter at Disney TV Animation from 2015-2018, 
-      and I have done illustration and visual development work for clients/projects 
-      that include Dreamworks TV, Disney Publishing, Lucasfilm Press, 
-      Dark Horse Comics, Marvel Snap, Hasbro, Wizards of the Coast 
-      (Magic the Gathering), Harper Collins, Amazon Audible, and many more 
-      publishers. I do not sell my work as NFT’s and do not condone any sale 
-      of my work as NFT’s. Any NFT you may find online made from my artwork 
-      are fraudulent and infringing on my copyright, in violation of the law. I do 
-      not sell my work on any other websites besides ones 
-      linked under my own name. </p>
-      <div className="about-wel title">
-        <h2>Let's Connect!</h2>
-      </div>
-      <p>Feel free to email me for professional freelance inquiries only, 
-      at fionahsiehart@gmail.com</p>
-      <p>For shop-related inquiries and questions about your order, 
-      please email help@whitesquirrel.com</p>
-      <p>My only online platforms are the ones 
-      linked through my linktree: https://linktr.ee/lunaurum 
-      I also have an old Deviantart account (fionahsieh), and am 
-      represented by my agent, Shannon Associates, on their website. </p>
-      <p>All other accounts claiming to be me are impersonators/fake accounts 
-      that are not authorized by me or any agent representing me.</p>
-      <div className="about-wel">
-        <h2 className="ques">More Questions?</h2>
-        <a href="/qa" className="faq">See our FAQs</a>
-      </div>
+    <Fade>
+      <div id="about" className="about">
+        <div className="about-wel">
+          <h1 className="top-about">About Me</h1>
+        </div>
+        <div className="about-images">
+          <img className="about-pic" alt="mews" src="../luna_images/latios.jpg"></img>
+          <img className="about-pic" alt="groudon" src="../luna_images/groudon.jpg"></img>
+          <img className="about-pic" alt="latias" src="../luna_images/latias.jpg"></img>
+        </div>
+        <div className="about-text">
+          <h1 className='about-header title'>Who Am I?</h1>
+          <p>I am a full-time independent illustrator and designer. 
+          I also freelance for publishing, animation, and games.</p>
+          <div className="about-wel title">
+            <h2>Things You May Know Me From...</h2>
+          </div>
+          <p>I was a background painter at Disney TV Animation from 2015-2018, 
+          and I have done illustration and visual development work for clients/projects 
+          that include Dreamworks TV, Disney Publishing, Lucasfilm Press, 
+          Dark Horse Comics, Marvel Snap, Hasbro, Wizards of the Coast 
+          (Magic the Gathering), Harper Collins, Amazon Audible, and many more 
+          publishers. I do not sell my work as NFT’s and do not condone any sale 
+          of my work as NFT’s. Any NFT you may find online made from my artwork 
+          are fraudulent and infringing on my copyright, in violation of the law. I do 
+          not sell my work on any other websites besides ones 
+          linked under my own name. </p>
+          <div className="about-wel title">
+            <h2>Let's Connect!</h2>
+          </div>
+          <p>Feel free to email me for professional freelance inquiries only, 
+          at fionahsiehart@gmail.com</p>
+          <p>For shop-related inquiries and questions about your order, 
+          please email help@whitesquirrel.com</p>
+          <p>My only online platforms are the ones 
+          linked through my linktree: https://linktr.ee/lunaurum 
+          I also have an old Deviantart account (fionahsieh), and am 
+          represented by my agent, Shannon Associates, on their website. </p>
+          <p>All other accounts claiming to be me are impersonators/fake accounts 
+          that are not authorized by me or any agent representing me.</p>
+          <div className="about-wel">
+            <h2 className="ques">More Questions?</h2>
+            <a href="/qa" className="faq">See our FAQs</a>
+          </div>
+        </div>
 
-    </div>
+      </div>
+    </Fade>
     <Footer />
     </>
   )
--- app/qa/page.js
+++ app/qa/page.js
@@ -1,52 +1,57 @@
+"use client";
 import React from 'react';
 import Navbar from "../navbar";
 import "./page.css";
 import Footer from "../footer";
+import Fade from 'react-reveal/Fade';
+
 
 export default function Qa() {
   return (
     <>
     <Navbar/>
-    <div className="terms">
+    <Fade>
+      <div className="terms">
 
-      <div className="faqs">
-          <h1 className="terms-header title">Terms and Conditions</h1>
-          <p>Terms and Conditions are accepeted 
-          automatically when a purchase is made.
-          </p>
-          <h2 className="title">Shipping and Handling</h2>
-          <li>Orders are shipped out within 1-2 weeks of purchase. 
-          However, there may be times when shipping takes longer. 
-          During holidays, shipping is expected to take longer.</li>
-          <h2 className="title">Help! I Entered The Wrong Address</h2>
-          <li>If the item has not been shipping, 
-          please email help@whitesquirrel.com to update your address</li>
-          <li>If the item has shipped, an additional payment is required.</li>
-          <h2 className="title">Missing packages?</h2>
-          <li>We do not accept responsibility for missing packages. 
-          Once the package status is "Delivered", it is out of our hands.
-          Please contact your shipping carrier </li>
-          <h2 className="title">Return Policy</h2>
-          <li>All sales are final once a product has been shipped.</li>
-          <h2 className="title">Damaged Goods</h2>
-          <li>Please contact us with photos of the product.</li>
-          <h2 className="title">Cancellations</h2>
-          <li>Orders are canceled if the order has not been 
-          shipped out.</li>
+        <div className="faqs">
+            <h1 className="terms-header title">Terms and Conditions</h1>
+            <p>Terms and Conditions are accepeted 
+            automatically when a purchase is made.
+            </p>
+            <h2 className="title">Shipping and Handling</h2>
+            <li>Orders are shipped out within 1-2 weeks of purchase. 
+            However, there may be times when shipping takes longer. 
+            During holidays, shipping is expected to take longer.</li>
+            <h2 className="title">Help! I Entered The Wrong Address</h2>
+            <li>If the item has not been shipping, 
+            please email help@whitesquirrel.com to update your address</li>
+            <li>If the item has shipped, an additional payment is required.</li>
+            <h2 className="title">Missing packages?</h2>
+            <li>We do not accept responsibility for missing packages. 
+            Once the package status is "Delivered", it is out of our hands.
+            Please contact your shipping carrier </li>
+            <h2 className="title">Return Policy</h2>
+            <li>All sales are final once a product has been shipped.</li>
+            <h2 className="title">Damaged Goods</h2>
+            <li>Please contact us with photos of the product.</li>
+            <h2 className="title">Cancellations</h2>
+            <li>Orders are canceled if the order has not been 
+            shipped out.</li>
+        </div>
+        <div className="questions">
+          <h1 className="title ans-ques">Frequently Asked Questions</h1>
+          <h2 className="ques">Q: Are you currently taking commissions / 
+          custom paintings / tattoo designs?</h2>
+          <li className="ans">A: Unfortunately, I am not currently doing commissions 
+          for individuals, as I am too busy with freelance work. I am only 
+          open to freelance work for publishers, games, and animation.</li>
+          <h2 className="ques">Q: Can I ship my MtG cards or other products for you to sign and ship back?</h2>
+          <li className="ans">A: Unfortunately, I'm not currently doing signings at home, 
+          only in-person signings at conventions. But I do very much appreciate 
+          your interest! </li>
+        </div>
       </div>
-      <div className="questions">
-        <h1 className="title ans-ques">Frequently Asked Questions</h1>
-        <h2 className="ques">Q: Are you currently taking commissions / 
-        custom paintings / tattoo designs?</h2>
-        <li className="ans">A: Unfortunately, I am not currently doing commissions 
-        for individuals, as I am too busy with freelance work. I am only 
-        open to freelance work for publishers, games, and animation.</li>
-        <h2 className="ques">Q: Can I ship my MtG cards or other products for you to sign and ship back?</h2>
-        <li className="ans">A: Unfortunately, I'm not currently doing signings at home, 
-        only in-person signings at conventions. But I do very much appreciate 
-        your interest! </li>
-      </div>
-    </div>
+    </Fade>
     <Footer />
   </>
   )
