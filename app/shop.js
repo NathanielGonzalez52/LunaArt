f128f7cdbd96df976aaf116ae4b678ea56a99916
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2023-12-20 21:41:01 -0800

    Add responsiveness to front page

--- app/shop.js
+++ app/shop.js
@@ -8,13 +8,13 @@ export default function shop() {
       <h1>Shop Collections</h1>
       <h1>Check out our unique pins and prints!</h1>
     </div>
-    <div className="merch-cont">
+    {/* <div className="merch-cont"> */}
         <div className="merch-pics">
             <img alt='merch' className="product" src="../luna_images/arceus.jpg"></img>
             <img alt='merch' className="product" src="../luna_images/rayquaza.jpg"></img>
         </div>
         <a href="/sale" className="to-shop">Shop Now</a>
-    </div>
+    {/* </div> */}
     </>
   )
 }
